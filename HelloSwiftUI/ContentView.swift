f0a726990f39a8079c5bcd187c7ea58fb1486722
Juan Gamino <gamino_juan03@hotmail.com>
2020-04-04 17:22:42 -0600

    Update ContentView.swift

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,11 +8,26 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+   
+    let students = ["John","Doe","Alice"]
+    @State private var selectStudent = 0
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            Form{
+                Picker("Select your student", selection: $selectStudent){
+                    ForEach(0 ..< students.count){
+                        Text(self.students[$0])
+                    }
+                }
+                Text("You chose: Student #\(students[selectStudent])")
+            }
+        }
+      
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
